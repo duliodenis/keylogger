991291aa00d78538737740eb2ec0a42dd4accaa9
Dulio Denis <dulio.denis@yahoo.com>
2016-09-06 23:33:56 -0400

    Added a Helper header file

---
+++ keylogger/Helper.h
@@ -0,0 +1,40 @@
+//
+//  KeyLogger: Windows based keystroke logger
+//  Helper.h
+//
+//  Created by Dulio Denis on 9/6/16.
+//  Copyright (c) 2016 Dulio Denis. All rights reserved.
+//
+//  Compiler: GCC g++
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <ctime>
+#include <string>
+#include <sstream>
+
+// Create a namespace
+namespace Helper {
+
+    template <class T>
+
+    // helper to convert into strings
+    std::string ToString(const T &);
+
+    // helper struct used to represent Date & Time
+    struct DateTime {
+
+        // Constructor
+        DateTime (){
+            time_t ms;
+            time(&ms);
+
+            struct tm * info = localtime(&ms);
+
+            D = info->tm_today;
+            m = info->tm_mon + 1;
+        }
+    };
+}
+
+#endif // HELPER_H
